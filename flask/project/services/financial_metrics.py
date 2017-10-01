a6f5ab35b32e0ec6b32c4a97155ce41ceed21e92
Radu-Raicea <radu@raicea.com>
2017-10-01 13:09:37 -0400

    add D/E ratio and net profit margin calculations

--- flask/project/services/financial_metrics.py
+++ flask/project/services/financial_metrics.py
@@ -15,6 +15,8 @@ class Stock(GoogleFinance):
         self.current_ratio = self._compute_current_ratio()
         self.quick_ratio = self._compute_quick_ratio()
         self.return_on_equity = self._compute_return_on_equity()
+        self.debt_equity_ratio = self._compute_debt_equity_ratio()
+        self.net_profit_margin = self._compute_net_profit_margin()
 
     def _compute_current_ratio(self):
         if self.bal and self.bal[10][0] == 'Total Current Assets' and self.bal[23][0] == 'Total Current Liabilities':
@@ -31,7 +33,23 @@ class Stock(GoogleFinance):
     def _compute_shareholders_equity(self):
         if self.bal and self.bal[17][0] == 'Total Assets' and self.bal[31][0] == 'Total Liabilities':
             return self.bal[17][1] - self.bal[31][1]
+        else:
+            return None
 
     def _compute_return_on_equity(self):
         if self.inc and self.inc[25][0] == 'Net Income':
             return self.inc[25][1] - self._compute_shareholders_equity()
+        else:
+            return None
+
+    def _compute_debt_equity_ratio(self):
+        if self.bal and self.bal[31][0] == 'Total Liabilities':
+            return self.bal[31][1] / self._compute_shareholders_equity()
+        else:
+            return None
+
+    def _compute_net_profit_margin(self):
+        if self.inc and self.inc[25][0] == 'Net Income' and self.inc[3][0] == 'Total Revenue':
+            return self.inc[25][1] / self.inc[3][1]
+        else:
+            return None
--- flask/tests/test_financial_metrics.py
+++ flask/tests/test_financial_metrics.py
@@ -6,41 +6,75 @@ from project.services.financial_metrics import Stock
 
 class TestFinancialMetrics(BaseTestCase):
 
-    def test_current_ratio_success(self):
+    def test_generate_metrics(self):
         with self.client:
             stock = Stock('NASDAQ', 'TSLA')
             stock.generate_metrics()
             self.assertTrue(stock.current_ratio)
+            self.assertTrue(stock.quick_ratio)
+            self.assertTrue(stock.return_on_equity)
+            self.assertTrue(stock.debt_equity_ratio)
+            self.assertTrue(stock.net_profit_margin)
+
+    def test_current_ratio_success(self):
+        with self.client:
+            stock = Stock('NASDAQ', 'TSLA')
+            self.assertTrue(stock._compute_current_ratio())
 
     def test_current_ratio_fail(self):
         with self.client:
             stock = Stock('NASDAQ', 'TSLAA')
-            stock.generate_metrics()
-            self.assertFalse(stock.current_ratio)
+            self.assertFalse(stock._compute_current_ratio())
 
     def test_quick_ratio_success(self):
         with self.client:
             stock = Stock('NASDAQ', 'TSLA')
-            stock.generate_metrics()
-            self.assertTrue(stock.quick_ratio)
+            self.assertTrue(stock._compute_quick_ratio())
 
     def test_quick_ratio_fail(self):
         with self.client:
             stock = Stock('NASDAQ', 'TSLAA')
-            stock.generate_metrics()
-            self.assertFalse(stock.quick_ratio)
+            self.assertFalse(stock._compute_quick_ratio())
+
+    def test_shareholders_equity_success(self):
+        with self.client:
+            stock = Stock('NASDAQ', 'TSLA')
+            self.assertTrue(stock._compute_shareholders_equity())
+
+    def test_shareholders_equity_fail(self):
+        with self.client:
+            stock = Stock('NASDAQ', 'TSLAA')
+            self.assertFalse(stock._compute_shareholders_equity())
 
     def test_return_on_equity_success(self):
         with self.client:
             stock = Stock('NASDAQ', 'TSLA')
-            stock.generate_metrics()
-            self.assertTrue(stock.return_on_equity)
+            self.assertTrue(stock._compute_return_on_equity())
 
     def test_return_on_equity_fail(self):
         with self.client:
             stock = Stock('NASDAQ', 'TSLAA')
-            stock.generate_metrics()
-            self.assertFalse(stock.return_on_equity)
+            self.assertFalse(stock._compute_return_on_equity())
+
+    def test_debt_equity_ratio_success(self):
+        with self.client:
+            stock = Stock('NASDAQ', 'TSLA')
+            self.assertTrue(stock._compute_debt_equity_ratio())
+
+    def test_debt_equity_ratio_fail(self):
+        with self.client:
+            stock = Stock('NASDAQ', 'TSLAA')
+            self.assertFalse(stock._compute_debt_equity_ratio())
+
+    def test_net_profit_margin_success(self):
+        with self.client:
+            stock = Stock('NASDAQ', 'TSLA')
+            self.assertTrue(stock._compute_net_profit_margin())
+
+    def test_net_profit_margin_fail(self):
+        with self.client:
+            stock = Stock('NASDAQ', 'TSLAA')
+            self.assertFalse(stock._compute_net_profit_margin())
 
 if __name__ == '__main__':
     unittest.main()
