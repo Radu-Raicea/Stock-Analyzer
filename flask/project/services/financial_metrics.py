46ea23093b398d3d5bccaf2584f7f40df828ce80
Radu-Raicea <radu@raicea.com>
2017-10-01 22:08:50 -0400

    round decimals and add remove button

--- flask/project/services/financial_metrics.py
+++ flask/project/services/financial_metrics.py
@@ -1,5 +1,6 @@
 
 from project.services.google_finance import GoogleFinance
+from decimal import ROUND_HALF_UP, Decimal
 
 
 class Stock(GoogleFinance):
@@ -21,10 +22,14 @@ class Stock(GoogleFinance):
         self.free_cash_flow = self._compute_free_cash_flow()
         self.price_to_earnings_ratio = self._get_price_to_earnings_ratio()
 
+    @staticmethod
+    def _round_decimal(number):
+        return number.quantize(Decimal('.01'), rounding=ROUND_HALF_UP)
+
     def _compute_current_ratio(self):
         try:
             if self.bal[10][0] == 'Total Current Assets' and self.bal[23][0] == 'Total Current Liabilities':
-                return self.bal[10][1] / self.bal[23][1]
+                return self._round_decimal(self.bal[10][1] / self.bal[23][1])
             else:
                 return None
         except Exception as e:
@@ -33,7 +38,7 @@ class Stock(GoogleFinance):
     def _compute_quick_ratio(self):
         try:
             if self.bal[10][0] == 'Total Current Assets' and self.bal[7][0] == 'Total Inventory' and self.bal[23][0] == 'Total Current Liabilities':
-                return (self.bal[10][1] - self.bal[7][1]) / self.bal[23][1]
+                return self._round_decimal((self.bal[10][1] - self.bal[7][1]) / self.bal[23][1])
             else:
                 return None
         except Exception as e:
@@ -42,7 +47,7 @@ class Stock(GoogleFinance):
     def _compute_shareholders_equity(self):
         try:
             if self.bal[17][0] == 'Total Assets' and self.bal[31][0] == 'Total Liabilities':
-                return self.bal[17][1] - self.bal[31][1]
+                return self._round_decimal(self.bal[17][1] - self.bal[31][1])
             else:
                 return None
         except Exception as e:
@@ -51,7 +56,7 @@ class Stock(GoogleFinance):
     def _compute_return_on_equity(self):
         try:
             if self.inc[25][0] == 'Net Income':
-                return self.inc[25][1] - self._compute_shareholders_equity()
+                return self._round_decimal(self.inc[25][1] - self._compute_shareholders_equity())
             else:
                 return None
         except Exception as e:
@@ -60,7 +65,7 @@ class Stock(GoogleFinance):
     def _compute_debt_equity_ratio(self):
         try:
             if self.bal[31][0] == 'Total Liabilities':
-                return self.bal[31][1] / self._compute_shareholders_equity()
+                return self._round_decimal(self.bal[31][1] / self._compute_shareholders_equity())
             else:
                 return None
         except Exception as e:
@@ -69,7 +74,7 @@ class Stock(GoogleFinance):
     def _compute_net_profit_margin(self):
         try:
             if self.inc[25][0] == 'Net Income' and self.inc[3][0] == 'Total Revenue':
-                return self.inc[25][1] / self.inc[3][1]
+                return self._round_decimal(self.inc[25][1] / self.inc[3][1])
             else:
                 return None
         except Exception as e:
@@ -78,7 +83,7 @@ class Stock(GoogleFinance):
     def _compute_free_cash_flow(self):
         try:
             if self.cas[7][0] == 'Cash from Operating Activities' and self.cas[8][0] == 'Capital Expenditures':
-                return self.cas[7][1] + self.cas[8][1]
+                return self._round_decimal(self.cas[7][1] + self.cas[8][1])
             else:
                 return None
         except Exception as e:
